f82ea490c24e7d894cbce781325f6a9ecb1c7344
Marek Hilton <marek.hilton15@imperial.ic.ac.uk>
2024-05-20 20:22:14 +0100

    Tidied up and propagated attributes and alt text.

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -5,36 +5,32 @@ import qualified Data.Text as T
 import Text.Pandoc.Builder
 import System.Process
 import GHC.IO.Exception (ExitCode(ExitSuccess, ExitFailure))
-import Text.Printf (printf)
+import Text.Printf (printf, hPrintf)
 import System.IO.Temp
 import Text.Pandoc.JSON (toJSONFilter)
 
-data QRConfig = QRConfig { url :: String
-                         , title :: T.Text }
 
 
 qrFilter :: IO ()
 qrFilter = toJSONFilter $ qrFilter_ getTmpPath
 
 qrFilter_ :: IO String -> Inline -> IO Inline
-qrFilter_ getPath lnk@(Link (_,classes,_) _ (tgt,titleText)) = do
+qrFilter_ getPath lnk@(Link attr@(_,classes,_) alt (tgt,titleText)) = do
   if "qr" `elem` classes
   then do
-    generateQR getPath (QRConfig { url = T.unpack tgt , title = titleText})
+    path <- getPath
+    procHandle <- runCommand $ printf "qrencode -t SVG -o '%s' '%s'" path tgt
+    exitCode <- waitForProcess procHandle
+    case exitCode of
+      ExitSuccess -> do
+        return $ Image attr alt (T.pack path,titleText)
+      ExitFailure n -> do
+        error $ printf "Failed to create QR code with error %d: %s" path n
   else
     return lnk
 qrFilter_ _ inline = return inline
 
-generateQR :: IO String -> QRConfig -> IO Inline
-generateQR getPath config = do
-  path <- getPath
-  procHandle <- runCommand $ "qrencode -o -t SVG" ++ path ++ "'" ++ url config ++ "'"
-  exitCode <- waitForProcess procHandle
-  case exitCode of
-    ExitSuccess -> return $ Image nullAttr [] (T.pack path,title config)
-    ExitFailure n -> do
-      error $ printf "Failed to create QR code with error %d: %s" path n
 
 getTmpPath :: IO String
 getTmpPath = do
-  emptyTempFile "qr_codes" "qr.svg"
+  emptySystemTempFile "qr.svg"
