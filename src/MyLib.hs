aae1d8e4ed1617a42d3923e46b6c2c0a819bae35
Marek Hilton <marek.hilton15@imperial.ic.ac.uk>
2024-05-21 13:44:27 +0100

    Changed filter to work on image blocks rather than inlines.

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -5,30 +5,47 @@ import qualified Data.Text as T
 import Text.Pandoc.Builder
 import System.Process
 import GHC.IO.Exception (ExitCode(ExitSuccess, ExitFailure))
-import Text.Printf (printf, hPrintf)
+import Text.Printf (printf)
 import System.IO.Temp
 import Text.Pandoc.JSON (toJSONFilter)
 
 
+data QRConfig = QRConfig
+  { target :: T.Text
+  , title :: T.Text
+  , attributes :: Attr
+  , alternate :: [Inline] }
+    
 
 qrFilter :: IO ()
 qrFilter = toJSONFilter $ qrFilter_ getTmpPath
 
-qrFilter_ :: IO String -> Inline -> IO Inline
-qrFilter_ getPath lnk@(Link attr@(_,classes,_) alt (tgt,titleText)) = do
-  if "qr" `elem` classes
-  then do
-    path <- getPath
-    procHandle <- runCommand $ printf "qrencode -t SVG -o '%s' '%s'" path tgt
-    exitCode <- waitForProcess procHandle
-    case exitCode of
-      ExitSuccess -> do
-        return $ Image attr alt (T.pack path,titleText)
-      ExitFailure n -> do
-        error $ printf "Failed to create QR code with error %d: %s" path n
-  else
-    return lnk
-qrFilter_ _ inline = return inline
+qrFilter_ :: IO String -> Block -> IO Block
+qrFilter_ getPath blk =
+  case blk of
+  Figure figAttr capt [Plain [Image attr@(_,classes,_) alt (tgt,titleText)]]
+    | "qr" `elem` classes -> do
+      let config = QRConfig tgt titleText attr alt
+      qr <- generateQRImage getPath config
+      return $ Figure figAttr capt [Plain [qr]]
+  Para [Image attr@(_,classes,_) alt (tgt,titleText)]
+    | "qr" `elem` classes -> do
+      let config = QRConfig tgt titleText attr alt
+      qr <- generateQRImage getPath config
+      return $ Figure nullAttr emptyCaption [Plain [qr]]
+  _notLink ->
+    return blk
+
+generateQRImage :: IO String -> QRConfig -> IO Inline
+generateQRImage getPath config = do
+      path <- getPath
+      procHandle <- runCommand $ printf "qrencode -t SVG -o '%s' '%s'" path (target config)
+      exitCode <- waitForProcess procHandle
+      case exitCode of
+        ExitSuccess -> do
+          return $ Image (attributes config) (alternate config) (T.pack path,title config)
+        ExitFailure n -> do
+          error $ printf "Failed to create QR code with error %d: %s" path n
 
 
 getTmpPath :: IO String
